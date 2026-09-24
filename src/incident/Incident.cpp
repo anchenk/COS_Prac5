cacd992aba77548d609930d11c67254d75c9f4e0
unknown <marshadowh20@gmail.com>
2026-09-24 18:55:00 +0200

    Musa: implement Incident (constructor, destructor, getters, Observer pattern)

--- src/incident/Incident.cpp
+++ src/incident/Incident.cpp
@@ -1,43 +1,40 @@
 #include "../../include/incident/Incident.h"
+#include <algorithm>
+#include <stdexcept>
 
-// TODO(Musa): implement constructor, destructor (delete state_),
-// state delegation, and observer notify loop.
 Incident::Incident(const std::string& id, const std::string& type,
                      const std::string& location, int severity)
-    : id_(id), type_(type), location_(location), severity_(severity), state_(nullptr) {}
+    : id_(id), type_(type), location_(location), severity_(severity), state_(nullptr) {
+    if (severity < 1 || severity > 5) {
+        throw std::invalid_argument("Incident severity must be between 1 and 5");
+    }
+}
 
 Incident::~Incident() {
-    // TODO: delete state_;
-}
-
-bool Incident::advanceState() {
-    // TODO: delegate to state_->advance(*this)
-    return false;
-}
-
-void Incident::setState(IncidentState* newState) {
-    // TODO: delete old state_, assign newState
-    state_ = newState;
-}
-
-std::string Incident::currentStateName() const {
-    // TODO: return state_->name();
-    return "";
-}
-
-void Incident::attach(IIncidentObserver* observer) {
-    observers_.push_back(observer);
-}
-
-void Incident::detach(IIncidentObserver* observer) {
-    // TODO: remove from observers_
-}
-
-void Incident::notify(const std::string& reason) {
-    // TODO: loop observers_, call onIncidentChanged(*this, reason)
+    delete state_;
+    // observers_ are NOT owned by Incident - they're external listeners
+    // (loggers, dashboards) that outlive individual incidents.
 }
 
 std::string Incident::getId() const { return id_; }
 std::string Incident::getType() const { return type_; }
 std::string Incident::getLocation() const { return location_; }
 int Incident::getSeverity() const { return severity_; }
+
+void Incident::attach(IIncidentObserver* observer) {
+    observers_.push_back(observer);
+}
+
+void Incident::detach(IIncidentObserver* observer) {
+    observers_.erase(
+        std::remove(observers_.begin(), observers_.end(), observer),
+        observers_.end()
+    );
+}
+
+void Incident::notify(const std::string& reason) {
+    for (std::vector<IIncidentObserver*>::iterator it = observers_.begin();
+         it != observers_.end(); ++it) {
+        (*it)->onIncidentChanged(*this, reason);
+    }
+}
